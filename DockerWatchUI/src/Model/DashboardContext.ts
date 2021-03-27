779eb6f3b9e1f5e206990c5b7434530ae21a35dd
ridiculous_rj <rahulrkj1@gmail.com>
2021-03-27 11:05:31 +0530

    live logs has been implemented

--- DockerWatchUI/src/Model/DashboardContext.ts
+++ DockerWatchUI/src/Model/DashboardContext.ts
@@ -4,7 +4,8 @@ import { Host } from './Host';
 import { HTTPService } from 'src/Services/HttpService';
 import { MessagePattern } from './MessagePattern';
 import { MessageStatus } from './MessageStatus';
-import { HostLogs } from './HostLogs';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 import { AppConfig } from 'src/app/app.config';
 
 export class DashboardContext{
@@ -18,9 +19,9 @@ export class DashboardContext{
 
     EditBoxHost: Host = new Host();
 
-    logs: string;
-
     httpService:HTTPService;
+
+    logs: string ="";
   
     constructor(hosts: Host[], images: Image[], containers:Container[], httpService: HTTPService) {
       this.hosts = hosts;
@@ -38,19 +39,15 @@ export class DashboardContext{
 
     ConnectHost(host: Host){
       if (this.selectedHost && this.selectedHost.id === host.id) {
-        HostLogs.DisconnectHost();
-        this.logs = "";
+        this._disconnect();
       } else {
         this.selectedHost = host;
-        this.logs = "";
-        HostLogs.ConnectHost(this.UpdateLogs);
         this.GetContainer(this.selectedHost);
         this.GetImages(this.selectedHost);
+        this._connect()
       }
     }
-    UpdateLogs(message: any){
-      this.logs += message;
-    }
+
     EditSelectedHost(){
       this.EditBoxHost = this.selectedHost;
     }
@@ -79,6 +76,53 @@ export class DashboardContext{
     async GetContainer(host: Host) {
       this.httpService.post<Container[]>(AppConfig.Address.GetContainers, host).subscribe(containers => {
         this.Containers = containers;
+        this.SelectContainer(this.Containers[0]);
       });
+    }
+
+    webSocketEndPoint: string = AppConfig.Address.GetLogs;
+    topic: string = AppConfig.Address.LogTopic;
+    stompClient: any;
+
+    _connect() {
+        console.log("Initialize WebSocket Connection");
+        let ws = new SockJS(this.webSocketEndPoint);
+        this.stompClient = Stomp.over(ws);
+        const _this = this;
+        _this.stompClient.connect({}, function (frame: any) {
+            _this.stompClient.subscribe(_this.topic, function (sdkEvent:any) {
+                _this.onMessageReceived(sdkEvent);
+            });
+            //_this.stompClient.reconnect_delay = 2000;
+        }, this.errorCallBack);
+    };
+
+    _disconnect() {
+        if (this.stompClient !== null) {
+            this.stompClient.disconnect();
+        }
+        console.log("Disconnected");
+    }
+
+    // on error, schedule a reconnection attempt
+    errorCallBack(error: any) {
+        console.log("errorCallBack -> " + error)
+        setTimeout(() => {
+            this._connect();
+        }, 5000);
+    }
+
+	/**
+	 * Send message to sever via web socket
+	 * @param {*} message
+	 */
+    _send(message: any) {
+        console.log("calling logout api via web socket");
+        this.stompClient.send(AppConfig.Address.SendLogMessage, {}, JSON.stringify(message));
+    }
+
+    onMessageReceived(message: any) {
+        console.log("Message Recieved from Server :: " + message);
+        this.logs += message.body;
     }
   }
